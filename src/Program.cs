6c81fad68445c85a97848c5637acedb11eebae15
fran <fran.sl@atlascity.io>
2020-07-10 11:50:25 +0100

    add manual config, update submodule and package versions

--- src/Program.cs
+++ src/Program.cs
@@ -1,30 +1,36 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using Microsoft.EntityFrameworkCore.Internal;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Utilities;
 using Org.BouncyCastle.Utilities.Encoders;
 using Org.BouncyCastle.Math.EC.Rfc8032;
 
-
-
 namespace CryptoBenchmarks
 {
-    
-    
-
-    
-    
     public class Program
     {
         public static void Main(string[] args)
-        {   
-           BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-            /*var sum2 = BenchmarkRunner.Run<Secp256k1Native>();*/
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new AllowNonOptimized());
         }
 
+        private class AllowNonOptimized : ManualConfig
+        {
+            public AllowNonOptimized()
+            {
+                AddValidator(JitOptimizationsValidator.DontFailOnError);
+                AddLogger(DefaultConfig.Instance.GetLoggers().ToArray());
+                AddExporter(DefaultConfig.Instance.GetExporters().ToArray()); 
+                AddColumnProvider(DefaultConfig.Instance.GetColumnProviders().ToArray());
+            }
+        }
     }
 }
 
